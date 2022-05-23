3d3bd1812a0f17c77580af9904f658a5efa2bf69
tc-kyano <k.yano@timeconcier.jp>
2022-05-23 15:14:45 +0900

    簡易情報タブをコンポーネント化

--- pages/mypage/profile.vue
+++ pages/mypage/profile.vue
@@ -13,122 +13,7 @@
 
     <!-- 簡易情報 -->
     <div v-if="tab == 'basic'" class="form-fields">
-
-      <!-- 郵便番号 -->
-      <div class="form-field mb-3 pb-3 border-bottom">
-        <label for="field-zipcode" class="d-block m-0 py-2 form-label">
-          <span class="field-required badge bg-danger me-2">必須</span>郵便番号
-        </label>
-
-        <input
-          type="tel"
-          id="field-zipcode"
-          class="form-control field-input"
-          maxlength="8"
-          name="zip_code"
-          placeholder="例）781-8007"
-          v-model="profile.zip_code"
-        >
-        <!--
-          @input="insertHyphen($event.target.value)"
-          @change="getAddressByZipcode($event.target.value)"
-        -->
-        <div>
-          <input
-            disabled
-            type="text"
-            v-model="profile.address"
-            class="form-control field-input mt-1"
-          >
-          <!-- <span v-else class="mt-1 text-danger">住所が取得できませんでした</span> -->
-        </div>
-      </div>
-
-      <!-- 性別 -->
-      <div class="form-field mb-3 pb-3 border-bottom">
-        <label class="d-block m-0 py-2 form-label">
-          <span class="field-required badge bg-danger me-2">必須</span>
-          <span>性別</span>
-        </label>
-
-        <div class="btn-group w-100 field-input">
-          <input
-            id="gender-2"
-            type="radio"
-            name="gender"
-            class="btn-check"
-            value="男性"
-            v-model="profile.gender"
-          >
-          <label class="btn btn-outline-primary" for="gender-2">男性</label>
-
-          <input
-            type="radio"
-            class="btn-check"
-            name="gender"
-            id="gender-3"
-            value="女性"
-            v-model="profile.gender"
-          >
-          <label class="btn btn-outline-primary" for="gender-3">女性</label>
-        </div>
-      </div>
-
-      <!-- 生年月日 -->
-      <div class="form-field mb-3 pb-3 border-bottom">
-        <label class="d-block m-0 py-2 form-label">
-          <span class="field-required badge bg-danger me-2">必須</span>
-          <span>生年月日</span>
-        </label>
-
-        <div class="input-group mb-1 field-input">
-          <select
-            class="form-select"
-            v-model="birthday.year"
-            name="birth_year"
-          >
-            <option value="0" selected>---</option>
-            <option
-              v-for="yyyy of $dayjs().format('YYYY') - 100"
-              :key="yyyy"
-              :value="yyyy"
-            >{{ yyyy }}年</option>
-          </select>
-
-          <select class="form-select" v-model="birthday.month" name="birth_month">
-            <option value="0" selected>---</option>
-            <option
-              v-for="m of 12"
-              :key="m"
-              :value="m"
-            >{{ m }}月</option>
-          </select>
-
-          <select class="form-select" v-model="birthday.date" name="birth_date">
-            <option value="0" selected>---</option>
-            <option
-              v-for="d of Number($dayjs($dayjs('2022/06/22').endOf('m')).format('D'))"
-              :key="d"
-              :value="d"
-            >{{ d }}日</option>
-          </select>
-        </div>
-      </div>
-
-      <!-- 職業 -->
-      <div class="form-field mb-3 pb-3 border-bottom">
-        <label class="d-block m-0 py-2 form-label">
-          <span>職業</span>
-
-          <select class="form-select">
-            <option
-              v-for="(job, index) in jobs"
-              :key="'job' + index"
-              :value="job"
-            >{{ job }}</option>
-          </select>
-        </label>
-      </div>
+      <basic :mY="mY"></basic>      
     </div>
 
 
@@ -360,18 +245,6 @@ export default {
       ],
       tab: 'basic',
 
-      profile: {
-        zip_code: '',
-        address: '',
-        gender: '',
-      },
-
-      birthday: {
-        year: '',
-        month: '',
-        day: '',
-      },
-
       details: {
         name: '',
         address1: '',
@@ -389,9 +262,6 @@ export default {
           '女性': ['長女', '次女', '三女', '四女', '五女', '六女', '七女', '八女', '九女', '十女'],
         }
       },
-
-      // 職業一覧
-      jobs: [ '会社員', '公務員', '自営業', '会社役員', '主婦(夫)', 'パート・アルバイト', '無職', 'シルバー世代', 'その他' ]
     }
   },
   created: function() {
@@ -399,7 +269,7 @@ export default {
   },
   mounted: async function() {
     const day = this.$dayjs(new Date());
-    this.mY = day.format('YYYY');
+    this.mY = Number(day.format('YYYY'));
   },
   methods: {
     async onSave() {
