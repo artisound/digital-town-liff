0663daa7f54cef7ebf567f15df768214b8a7b59f
tc-kyano <k.yano@timeconcier.jp>
2022-05-27 10:45:21 +0900

    性別入力欄を追加

--- pages/init/index.vue
+++ pages/init/index.vue
@@ -53,6 +53,42 @@
           <p class="invalid-feedback is-invalid d-block">郵便番号を入力してください</p>
         </template>
       </div>
+
+      <!-- 性別入力欄 -->
+      <div class="form-field mb-3 pb-3 border-bottom">
+        <label class="d-block m-0 py-2 form-label">
+          <span class="field-required badge bg-danger me-2">必須</span>
+          性別
+        </label>
+        <div class="btn-group w-100 field-input">
+          <input
+            type="radio"
+            id="gender-2"
+            class="btn-check"
+            name="gender"
+            value="男性"
+            v-model="inputData.gender"
+            autocomplete="off"
+          >
+          <label
+            for="gender-2"
+            class="btn btn-outline-primary"
+          >男性</label>
+          <input
+            type="radio"
+            id="gender-3"
+            class="btn-check"
+            name="gender"
+            value="女性"
+            v-model="inputData.gender"
+            autocomplete="off"
+          >
+          <label
+            for="gender-3"
+            class="btn btn-outline-primary"
+          >女性</label>
+        </div>
+      </div>
     </template>
   </div>
 </template>
