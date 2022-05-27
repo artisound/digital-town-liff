7257fd80c2fa21467f8ddb6d63135726801139ea
tc-kyano <k.yano@timeconcier.jp>
2022-05-27 10:43:13 +0900

    ようこそ表示を追加

---
+++ pages/init/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <!-- ようこそ -->
+    <template v-if="displayMode == 0">
+      <div
+        id="welcomeView"
+        class="form-fields px-2 text-center"
+      >
+        <h2 class="mt-5 mx-4 display-7 text-outline">ようこそ<br>デジタル町一丁目<br>[ とさジョブ ]</h2>
+        <div
+          class="position-absolute top-50 left-50 w-100"
+          style="transform: translate(-50%, -50%);"
+        >
+          <button
+            class="btn btn-outline-primary btn-lg bg-white"
+            :disabled="btnStart"
+            @click="btnAction($event, 'start')"
+          >はじめる</button>
+        </div>
+      </div>
+    </template>
+  </div>
+</template>
