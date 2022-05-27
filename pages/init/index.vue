bd964aa492c117c9cebf97326732ed08b25cd78f
tc-kyano <k.yano@timeconcier.jp>
2022-05-27 10:44:59 +0900

    郵便番号入力欄を追加

--- pages/init/index.vue
+++ pages/init/index.vue
@@ -28,6 +28,31 @@
         </span>
         <span class="h2 fw-bold text-center">新規登録</span>
       </div>
+      
+      <!-- 郵便番号入力欄 -->
+      <div class="form-field mb-3 pb-3 border-bottom">
+        <label
+          for="zip_code"
+          class="d-block m-0 py-2 form-label"
+        >
+          <span class="field-required badge bg-danger me-2">必須</span>
+          郵便番号
+        </label>
+        <input
+          type="tel"
+          id="zip_code"
+          class="form-control field-input"
+          maxlength="8"
+          v-model="$v.inputData.$model.zip_code"
+          @input="addHyphenToZipcode($event.target.value);"
+          @change="$v.inputData.zip_code.$touch();"
+          name="zip_code"
+          placeholder="例）781-8007"
+        >
+        <template v-if="$v.inputData.zip_code.$error">
+          <p class="invalid-feedback is-invalid d-block">郵便番号を入力してください</p>
+        </template>
+      </div>
     </template>
   </div>
 </template>
